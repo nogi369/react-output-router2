2215e9bf28b84b40b6ff5834ec102fd18b5ff65d
nogi <Createc369.Re@gmail.com>
2023-10-19 09:50:51 +0900

    add: react-router-dom

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-output-first-4",
+  base: "/react-output-router2",
   test: {
     globals: true,
     environment: "jsdom",
